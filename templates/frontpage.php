bb03795f32a8d84f180f29a195f9f46afb468cfd
Younes Ben Dhiab <87871087+bendhiab-younes@users.noreply.github.com>
2023-10-21 00:28:13 +0100

    data filter

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,32 +1,39 @@
 <?php include 'inc/header.php'; ?>
 
 <!-- Frontpage Section -->
-<div class="content mt-3 p-4">
+<div class="container mt-3">
     <h1 class="page-title">Session</h1>
-    <h5 class="mb-4">Trié par année</h5>
-
-    <form method="GET" action="index.php" class="mb-4">
-        <div class="form-group">
-            <select title="select" name="Annee" class="form-control">
-                <option value="0">Choisir une année</option>
-                <?php foreach ($Annees as $Annee): ?>
-                    <option value="<?php echo $Annee->Numero; ?>">
-                        <?php echo $Annee->Annee; ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
+    <div class="row mb-4">
+        <div class="col-md-6">
+            <h5>Filter by Column</h5>
+            <div class="form-row">
+                <div class="col-md-6 mb-2">
+                    <select id="filter-column" class="form-control">
+                        <option value="Numero">Numero</option>
+                        <option value="Annee">Annee</option>
+                        <option value="Sem">Sem</option>
+                        <option value="Semab">Semab</option>
+                        <option value="Debut">Debut</option>
+                        <option value="Fin">Fin</option>
+                        <option value="Debsem">Debsem</option>
+                        <option value="Finsem">Finsem</option>
+                        <option value="Annea">Annea</option>
+                        <option value="Anneab">Anneab</option>
+                    </select>
+                </div>
+                <div class="col-md-6 mb-2">
+                    <input type="text" id="filter-value" class="form-control" placeholder="Enter a value">
+                </div>
+            </div>
         </div>
-        <br>
-        <button type="submit" class="btn btn-primary">Filter</button>
-    </form>
-
+    </div>
 
     <div class="table-responsive">
         <table class="table table-striped table-bordered">
             <thead>
                 <tr>
                     <th>Numero</th>
-                    <th>Anne</th>
+                    <th>Annee</th>
                     <th>Sem</th>
                     <th>Semab</th>
                     <th>Debut</th>
@@ -79,7 +86,77 @@
             </tbody>
         </table>
     </div>
+
+    <button id="generate-pdf" class="btn btn-primary">Generate PDF</button>
 </div>
+
+<!-- Add Bootstrap CSS and JavaScript -->
+
+
+<script>
+    const filterColumn = document.getElementById('filter-column');
+    const filterValue = document.getElementById('filter-value');
+    const tableRows = document.querySelectorAll('.table tbody tr');
+    const generatePdfButton = document.getElementById('generate-pdf');
+
+    filterColumn.addEventListener('change', () => {
+        filterData(filterColumn.value, filterValue.value);
+    });
+
+    filterValue.addEventListener('input', () => {
+        filterData(filterColumn.value, filterValue.value);
+    });
+
+    function filterData(column, value) {
+        const filterRegex = new RegExp(value, 'i');
+        tableRows.forEach(row => {
+            const cell = row.querySelector(`td:nth-child(${getColumnIndex(column)})`);
+            if (cell) {
+                if (filterRegex.test(cell.textContent)) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            }
+        });
+    }
+
+    function getColumnIndex(columnName) {
+        const headers = document.querySelectorAll('.table thead th');
+        for (let i = 0; i < headers.length; i++) {
+            if (headers[i].textContent.trim() === columnName) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+    generatePdfButton.addEventListener('click', () => {
+        const doc = new jsPDF();
+
+        const columns = [];
+        const tableHeaders = document.querySelectorAll('.table thead th');
+        tableHeaders.forEach(header => {
+            columns.push(header.textContent.trim());
+        });
+
+        const data = [];
+        tableRows.forEach(row => {
+            const rowData = [];
+            row.querySelectorAll('td').forEach(cell => {
+                rowData.push(cell.textContent.trim());
+            });
+            data.push(rowData);
+        });
+
+        doc.autoTable({
+            head: [columns],
+            body: data,
+        });
+
+        doc.save('sessions.pdf');
+    });
+</script>
 
 <!-- Footer Section -->
 <?php include 'inc/footer.php'; ?>
--- templates/inc/header.php
+++ templates/inc/header.php
@@ -3,31 +3,58 @@
 
 <head>
     <title>Header Sessions</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <?php
+    $urls = array();
+    $scripts = array(
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+        'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+        'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'
+    );
+    $stylesheets = array(
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+    );
+    foreach ($scripts as $script) {
+        if (!in_array($script, $urls)) {
+            echo '<script src="' . $script . '"></script>' . PHP_EOL;
+            $urls[] = $script;
+        }
+    }
+    foreach ($stylesheets as $stylesheet) {
+        if (!in_array($stylesheet, $urls)) {
+            echo '<link rel="stylesheet" href="' . $stylesheet . '">' . PHP_EOL;
+            $urls[] = $stylesheet;
+        }
+    }
+    ?>
+
     <link rel="stylesheet" href="css/styles.css">
 </head>
 
-<body >
+<body>
     <!-- Header Navigation -->
     <div>
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-4">
-            <a class="navbar-brand" href="index.php"><?php echo SITE_TITLE; ?></a>
+            <a class="navbar-brand" href="index.php">
+                <?php echo SITE_TITLE; ?>
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="create.php">Create</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</div>
-<?php displayMessage(); ?>
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="create.php">Create</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <?php displayMessage(); ?>
